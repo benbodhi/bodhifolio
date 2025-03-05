44e4b0e20eda3c9b81bffd7e68686439523db48f
Benbodhi <5193411+benbodhi@users.noreply.github.com>
2025-03-05 13:30:12 +1100

    swap bomb to noggles

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -5,9 +5,9 @@ export function Footer() {
   return (
     <footer className="h-[116px] bg-[hsl(var(--background))] mt-24 relative">
       <div className="container h-16 mx-auto px-4 flex items-center justify-center z-10 relative">
-        {/* <Noggles /> */}
+        <Noggles />
       </div>
-      <RollingBomb />
+      {/* <RollingBomb /> */}
     </footer>
   )
 } 
